adead9d70d9340f2adbaf69f3ceaa36725c77ae9
Evgeniy <evsinev@gmail.com>
2013-08-18 01:16:43 +0400

    Added su files

---
+++ jni/Android.mk
@@ -0,0 +1,10 @@
+
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := su
+LOCAL_SRC_FILES := su.c
+
+include $(BUILD_EXECUTABLE)
---
+++ jni/make.sh
@@ -0,0 +1,5 @@
+
+/opt/android-ndk-r8d/ndk-build \
+  && adb push ../libs/armeabi/su /data/local/su 
+
+
